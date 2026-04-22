b7247a44030d53be47b776cba27613ed125b94aa
Amrit Kumar <amritsinghrajput7615@gmail.com>
2026-04-23 00:39:32 +0530

    fix: correct model file name casing2

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use(cors({
-  origin: "https://interview-platform1-nine.vercel.app",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
